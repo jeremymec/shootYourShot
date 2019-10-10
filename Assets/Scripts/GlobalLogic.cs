7d096c71a60855639b6746b929c3e82594a6861d
Jeremy Mechen <jeremymechen1997@gmail.com>
2019-10-11 03:25:39 +1300

    Changed Scene

--- Assets/Scripts/GlobalLogic.cs
+++ Assets/Scripts/GlobalLogic.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class GlobalLogic : MonoBehaviour
 {
+
+    public float weaponSpawnTime;
+    public bool weaponSpawn;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,17 @@ public class GlobalLogic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        weaponSpawnTime -= Time.deltaTime;
+
+        if (weaponSpawnTime <= 0.0f && weaponSpawn)
+        {
+            weaponSpawn = false;
+            this.spawnWeapons();
+        }
+    }
+
+    void spawnWeapons()
+    {
+
     }
 }
